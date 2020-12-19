c1ac9f4a3e5abd08fe1d05ed22510497f49a2d79
Yash Khemani <khemani@m13.local>
2020-12-19 01:54:14 +0000

    update for terraform 0.14

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+      version = "1.6.3"
+    }
+  }
+}
+
 provider "acme" {
   server_url = var.acme_server_url
 }
